67bff11dc01fb5c2f07748b8b3785bc03d18d88a
Matt Anderson <anderson.matthew.eric@gmail.com>
2022-01-12 02:00:49 -0800

    Add a bit more godoc

--- filesys/filesys.go
+++ filesys/filesys.go
@@ -5,7 +5,9 @@ import (
 	"github.com/manderson5192/memfs/inode"
 )
 
+// FileSystem represents an in-memory filesystem
 type FileSystem interface {
+	// RootDirectory returns a reference to the filesystem's root directory
 	RootDirectory() directory.Directory
 }
 
@@ -13,6 +15,7 @@ type fileSystem struct {
 	rootDirectory *inode.DirectoryInode
 }
 
+// NewFileSystem creates a new FileSystem instance based on an inode tree
 func NewFileSystem() FileSystem {
 	return &fileSystem{
 		rootDirectory: inode.NewRootDirectoryInode(),
